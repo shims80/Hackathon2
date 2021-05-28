b6b96523bece5e19bf90bf3b844cc6e74a2e584e
shims80 <shims80@gmail.com>
2021-05-28 12:32:07 +0300

    password update

--- AudioBook/app.py
+++ AudioBook/app.py
@@ -8,7 +8,7 @@ counter = 0
 def run_query(query, type):
     HOSTNAME = '127.0.0.1'
     USERNAME = 'postgres'
-    PASSWORD = 'PCC3MRAP'
+    PASSWORD = '********'
     DATABASE = 'text2audio'
     connection = psycopg2.connect(
         host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE)
